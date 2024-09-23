2fd6dcc99e9ec80a6a7bc2360beee230350e3932
vedant1325 <147478924+vedant1325@users.noreply.github.com>
2024-09-23 22:00:00 +0530

    Update SongItem.jsx

--- spotify-clone-frontend/src/Components/SongItem.jsx
+++ spotify-clone-frontend/src/Components/SongItem.jsx
@@ -5,7 +5,7 @@ const SongItem = ({name,id,image,desc}) => {
   const{playWithId}=useContext(PlayerContext)
   return (
     <div onClick={()=>{playWithId(id)}} className='min-w-[180px] p-2 x-3 rounded cursor-pointer hover:bg-[#ffffff26]'>
-        <img className='rounded' src={image} alt="" />
+        <img className='rounded p-1' src={image} alt="" />
         <p className='font-bold mt-2 mb-1'>{name}</p>
         <p className='text-slate-100 text-small'> {desc.slice(0,49)}...</p>
       
